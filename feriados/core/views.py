c3a229d8465e2903d4d8f128b2cb4156f3f8f8d6
vitorHerni <vitorsantos.info@gmail.com>
2022-06-22 21:42:49 -0300

    atividade web III

---
+++ feriados/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+from datetime import date
+from django.shortcuts import render
+from cadastro_api.models import FeriadoModel
+
+def verificar(request):
+    hoje = date.today()
+
+    try:
+        feriados = FeriadoModel.objects.all().filter(data=hoje)
+    except FeriadoModel.DoesNotExist:
+        feriados = None
+
+    msgs = [f'Hoje é feriado de {f}' for f in feriados] if feriados else ['Hoje não é feriado']
+
+    return render(request, 'feriado.html', {'msgs': msgs})
